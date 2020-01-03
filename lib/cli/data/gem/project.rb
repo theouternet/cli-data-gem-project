1adb68d6ba54618ad96c0776803c8c02af167697
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-01-03 23:11:50 +0000

    first commit

---
+++ lib/cli/data/gem/project.rb
@@ -0,0 +1,12 @@
+require "cli/data/gem/project/version"
+
+module Cli
+  module Data
+    module Gem
+      module Project
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/cli/data/gem/project/version.rb
@@ -0,0 +1,9 @@
+module Cli
+  module Data
+    module Gem
+      module Project
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
